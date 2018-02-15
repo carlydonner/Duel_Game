149e217109fa50ab0252a59299646ac3085826e2
TylerDavidson <tylerjdavidson2018@gmail>
2018-02-15 10:58:30 -0500

    First commit of player & arrow

---
+++ arrow.py
@@ -0,0 +1,30 @@
+import pygame
+from colors import *
+
+ARROW_SIZE = 10
+ARROW_WIDTH = 3
+
+class Arrow:
+	def __init__(self,x,y):
+		self.moveTo(x,y)
+        
+	def move(self, dt):
+		self.x += self.v_x*dt        
+
+	def moveTo(self, x, y):
+		self.x = x
+		self.y = y
+		self.v_x = 0
+        
+	def getRect(self):
+		r = pygame.Rect((0,0,ARROW_LENGTH,ARROW_WIDTH))
+		r.center = (self.x, self.y)
+		return r
+
+	def isMoving(self):
+		return not ((self.v_x==0))
+    
+	def draw(self, surf):
+		r = pygame.Rect((0,0,ARROW_LENGTH,ARROW_WIDTH))
+		r.center = (self.x, self.y)
+		pygame.draw.rect(surf, ARROW_COLOR, r)
---
+++ player.py
@@ -0,0 +1,28 @@
+import pygame
+import math
+from colors import *
+
+
+class Player:
+	def __init__(self,x,y):
+		self.x = x
+		self.y = y
+		self.angle = 45
+		self.mag = 50
+		self.width = 2
+		self.color = PLAYER_COLOR
+
+	def fire(self, arrow):
+		arrow.v_x = 50
+
+	def movePlayer(self,dx,dy)
+		self.x += dx
+		self.y += dy
+
+	def draw(self,surf):
+		dx = 20
+		dy = 20     
+		pygame.draw.aaline(surf, self.color,
+                      (self.x,self.y),
+                      (self.x+dx, self.y-dy),
+                       self.width)
