db9c9fef75225ae386892e15aaccd0c9cbc44dfc
TylerDavidson <tylerjdavidson2018@gmail>
2018-02-15 11:20:52 -0500

    Cleaned the repository

---
+++ target.py
@@ -0,0 +1,23 @@
+import pygame
+from colors import *
+
+HEIGHT = 10
+
+class Target:
+    def __init__(self,x,y,w):
+        self.rect =  pygame.Rect((0,y,w,HEIGHT))
+        self.rect.centerx = x
+        
+    def hitBy(self,rect):
+        return self.rect.colliderect(rect)
+        
+    def moveTo(self, x, y,w):
+        self.rect =  pygame.Rect((0,y,w,HEIGHT))
+        self.rect.centerx = x
+        
+
+
+
+    def draw(self,surf):
+        pygame.draw.rect(surf, TARGET_COLOR, self.rect)
+
